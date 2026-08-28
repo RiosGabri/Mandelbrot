b1eb6c351488ecbff75ff8324766a945e58ccd7d
RiosGabri <grp@cesar.school>
2026-08-28 09:10:57 -0300

    feat: Tratamentode erro na entrada de argumentos

--- agenda.md
+++ agenda.md
@@ -7,3 +7,10 @@
     - Lógica dos argumentos
     - Conversão inicial de argumento(string) para int
 ```
+
+```
+28/08 -> Substituição do atol pelo strtol para validação das entradas
+    - Implementação do errno == ERANGE (previne estouro)
+    - *endptr != '\0' (Pega caractere que parou de ler -> Se esse caractere não for o fim da string ('\0'), significa que o tem letras ou símbolos inválidos)
+    - argv[1] == endptr (vazio ou começa com letra)
+```
--- mandelbrot.c
+++ mandelbrot.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]){
-    int largura; 
-    int altura;
-    int max_iteracoes;
-    int num_threads; 
-
-    /*double real_min = -2.0;
-    double real_max = 1.0;
-    double imag_min = -1.5;
-    double imag_max = 1.5;*/
+    long largura; 
+    long altura;
+    long max_iteracoes;
+    long num_threads; 
+    char *endptr;
 
     if(argc != 5){
-        //mensagem de erro aqui
-        //exit();
+        fprintf(stderr, "Qtd de argumentos invalida\n");
+        exit(EXIT_FAILURE);
     }else{
-        largura = atoi(argv[1]);
-        altura = atoi(argv[2]);
-        max_iteracoes = atoi(argv[3]);
-        num_threads = atoi(argv[4]);        
+        errno = 0;
+        largura = strtol(argv[1], &endptr, 10);
+        if(*endptr != '\0' || argv[1] == endptr || largura <= 0 || errno == ERANGE){
+            fprintf(stderr, "largura invalida\n");
+            exit(EXIT_FAILURE);
+        }
+        errno = 0;
+        altura = strtol(argv[2], &endptr, 10);
+        if(*endptr != '\0' || argv[2] == endptr || altura <= 0 || errno == ERANGE){
+            fprintf(stderr, "altura invalida\n");
+            exit(EXIT_FAILURE);
+        }
+        errno = 0;
+        max_iteracoes = strtol(argv[3], &endptr, 10);
+        if(*endptr != '\0' || argv[3] == endptr || max_iteracoes <= 0 || errno == ERANGE){
+            fprintf(stderr, "numero de iteracoes invalido\n");
+            exit(EXIT_FAILURE);
+        }
+        errno = 0;
+        num_threads = strtol(argv[4], &endptr, 10);  
+        if(*endptr != '\0' || argv[4] == endptr || num_threads <= 0 || errno == ERANGE){
+            fprintf(stderr, "numero de threads invalido\n");
+            exit(EXIT_FAILURE);
+        }    
     }
 
     return 0;
