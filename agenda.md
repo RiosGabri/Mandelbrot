7927e1c5aff7f1370a2c6ad0d86d0dc7ed78af10
RiosGabri <grp@cesar.school>
2026-08-27 22:23:15 -0300

    add: Conversão inicial de argumento(string) para int

---
+++ agenda.md
@@ -0,0 +1,9 @@
+# Agenda
+
+```
+27/08 -> L0ǵica inicial:
+    - Declaração inicial das variáveis
+    - Análise do enunciado
+    - Lógica dos argumentos
+    - Conversão inicial de argumento(string) para int
+```
--- mandelbrot.c
+++ mandelbrot.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
     int largura; 
-    int altura; 
+    int altura;
+    int max_iteracoes;
+    int num_threads; 
 
-    double real_min = -2.0;
+    /*double real_min = -2.0;
     double real_max = 1.0;
     double imag_min = -1.5;
-    double imag_max = 1.5;
+    double imag_max = 1.5;*/
+
+    if(argc != 5){
+        //mensagem de erro aqui
+        //exit();
+    }else{
+        largura = atoi(argv[1]);
+        altura = atoi(argv[2]);
+        max_iteracoes = atoi(argv[3]);
+        num_threads = atoi(argv[4]);        
+    }
 
     return 0;
 }
