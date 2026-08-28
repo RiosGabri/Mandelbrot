288b1d899f94ea7cc316c923344015063a2ad05d
RiosGabri <grp@cesar.school>
2026-08-27 21:37:16 -0300

    add: Estrutura inicial

---
+++ mandelbrot.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int largura; 
+    int altura; 
+
+    double real_min = -2.0;
+    double real_max = 1.0;
+    double imag_min = -1.5;
+    double imag_max = 1.5;
+
+    return 0;
+}
