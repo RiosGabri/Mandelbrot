5b5dc57d522e4c6801fea78b0dfa5a3fbce6569d
RiosGabri <grp@cesar.school>
2026-08-29 19:35:27 -0300

    feat: implementação da openMP finalizda

--- mandelbrot.c
+++ mandelbrot.c
@@ -4,9 +4,10 @@
 #include <errno.h>
 #include "calculo.h"
 #include <time.h>
+#include <omp.h>
 
-void salvar_imagem(int *imagem, long largura, long altura) {
-    FILE *arquivo = fopen("mandelbrot_grp_serial.pgm", "w");
+void salvar_imagem(int *imagem, long largura, long altura, const char *nome_arquivo) {
+    FILE *arquivo = fopen(nome_arquivo, "w");
     if (arquivo == NULL) {
         fprintf(stderr, "erro ao abrir arquivo da imagem\n");
         exit(EXIT_FAILURE);
@@ -20,15 +21,18 @@ void salvar_imagem(int *imagem, long largura, long altura) {
     fclose(arquivo);
 }
 
-void salvar_tempo(double tempo) {
+void salvar_tempos(double tempo_serial, double tempo_openmp) {
     FILE *arquivo = fopen("times.txt", "w");
     if (arquivo == NULL) {
-        fprintf(stderr, "erro ao abrir arquivo de tempos\n");
+        fprintf(stderr, "Erro ao abrir arquivo de tempos\n");
         exit(EXIT_FAILURE);
     }
-    fprintf(arquivo, "serial: %.9f segundos\n", tempo);
+    fprintf(arquivo, "serial: %.9f segundos\n", tempo_serial);
+    fprintf(arquivo, "openmp: %.9f segundos\n", tempo_openmp);
+
     fclose(arquivo);
 }
+
 
 double calcular_tempo(struct timespec inicio, struct timespec fim) {
     double segundos = (double)(fim.tv_sec - inicio.tv_sec);
@@ -79,16 +83,22 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    struct timespec inicio;
-    struct timespec fim;
+    struct timespec inicio_serial;
+    struct timespec fim_serial;
 
-    clock_gettime(CLOCK_MONOTONIC, &inicio);
+    clock_gettime(CLOCK_MONOTONIC, &inicio_serial);
     ImagemSerial(imagem, largura, altura, max_iteracoes);
-    clock_gettime(CLOCK_MONOTONIC, &fim);
-    
-    double tempo = calcular_tempo(inicio, fim);
-    salvar_imagem(imagem, largura, altura);
-    salvar_tempo(tempo);
+    clock_gettime(CLOCK_MONOTONIC, &fim_serial);
+
+    double tempo_serial = calcular_tempo(inicio_serial, fim_serial);
+    salvar_imagem(imagem, largura, altura, "mandelbrot_grp_serial.pgm");
+
+    double inicio_openmp = omp_get_wtime();
+    ImagemOpenMP(imagem, largura, altura, max_iteracoes, num_threads);
+    double fim_openmp = omp_get_wtime();
+    double tempo_openmp = fim_openmp - inicio_openmp;
+    salvar_imagem(imagem,largura, altura, "mandelbrot_grp_openmp.pgm");
+    salvar_tempos(tempo_serial, tempo_openmp);
 
     free(imagem);
 
